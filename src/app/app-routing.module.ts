fe89b0ccf7e2c980760da3921551b23befbb4af3
sulaiman1015 <sulaiman1015@gmail.com>
2022-04-20 11:04:53 +0200

    modified routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent} from "./homepage/homepage.component";
+import {LoginpageComponent} from "./loginpage/loginpage.component";
+
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path:'home', component: HomepageComponent},
+  { path:'login', component: LoginpageComponent},
+  //{ path:'employee',loadChildren:'./employee/employee.module#EmployeeModule'},
+  { path:'employee',loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule)},
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EmpListComponent} from "./emp-list/emp-list.component";
+import {EmpAddComponent} from "./emp-add/emp-add.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'list', component:EmpListComponent},
+  {path:'add', component:EmpAddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
