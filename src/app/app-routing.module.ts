abdb7f6e4f73e1894a83d27df8f80c19c90a55b7
sulaiman1015 <sulaiman1015@gmail.com>
2022-04-20 12:05:44 +0200

    edited welcome page content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomepageComponent} from "./homepage/homepage.component";
 import {LoginpageComponent} from "./loginpage/loginpage.component";
+import {WelcomeComponent} from "./pages/welcome/welcome.component";
 
 
 const routes: Routes = [
@@ -9,8 +10,13 @@ const routes: Routes = [
   { path:'home', component: HomepageComponent},
   { path:'login', component: LoginpageComponent},
   //{ path:'employee',loadChildren:'./employee/employee.module#EmployeeModule'},
-  { path:'employee',loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule)},
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path:'',loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule)},
+  { path:'welcome',loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule)}
+  /*{ path: 'welcome', component:WelcomeComponent,
+  /!*loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule)*!/
+  children:[
+    { path:'employee',loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule)}
+  ]}*/
 ];
 
 @NgModule({
